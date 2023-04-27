0d4b79a99d983ce07b8dc263933557133f486ae6
Renuka-Jain <renuka.jain@estudiantat.upc.edu>
2023-04-27 16:42:05 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter here the message.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="/">
+        <textarea id="freeform" name="freeform" rows="4" cols="50"/><br></br>
+        <input type="submit" value="Submit"/>
+      </form>
       </header>
+      
     </div>
   );
 }
